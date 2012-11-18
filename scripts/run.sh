47897b467b4a54cb8a637eb1fa782cf16eb1a298
Owen Imholte <owenimholte@mac.com>
2012-11-18 00:45:23 -0800

    Fix this up so the cleanup command is in the right spot

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # A basic script to cat the logs and generate the output
+echo root is $ROOT;
 
 # Put the logs in one file
-cat $ROOT/sample/logs/* | $ROOT/cleanup.sh > $ROOT/sample/one-week-full-logs.txt
+cat $ROOT/sample/logs/* | $ROOT/scripts/cleanup.sh > $ROOT/sample/one-week-full-logs.txt
 
 # Create a databse
 
