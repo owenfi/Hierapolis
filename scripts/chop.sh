8070dd0fa7b5de76d9adac3093255bfc31443b84
Owen Imholte <github@owenimholte.com>
2012-12-23 02:05:14 -0600

    Adding a file that makes the lines into csv's ready for import to sql

---
+++ scripts/chop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# A smaller script to chop our normalized into various sub-table info
+
+# Definition of a line: (Space delimited)
+# RequestId BucketOwner BucketName Time RemoteIP Requester Operation Key "URI" HTTPStatus ErrorCode BytesSent ObjectSize TotalTime TurnaroundTime "Referrer" "User-Agent" VersionId
+
+# Regex to grab just the Remote IP Address:
+# sed 's/.* .* .* .* \(.*\) .* .* .* ".*" .* .* .* .* .* .* ".*" ".*" .*/\1/g'
+
+# Regex for: IP,Date,Object Name,Bytes Transferred,Total Bytes,ReqID
+sed 's/\(.*\) .* .* \(.*\) \(.*\) .* .* \(.*\) ".*" .* .* \(.*\) \(.*\) .* .* ".*" ".*" .*/\3,\2,\4,\5,\6,\1/g' |
+
+# Comment this if it's really slow?
+sort
