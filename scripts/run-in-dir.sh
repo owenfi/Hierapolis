61f55b841529a78b79b57dd42d1d1d4c2ee81ebd
Owen Imholte <github@owenimholte.com>
2013-03-02 23:56:08 -0800

    Adding a version to be run in a given directory and dumps out processed text file. This version handles large numbers of logs better

---
+++ scripts/run-in-dir.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# A basic script to cat the logs and generate the output
+echo root is $ROOT;
+
+# Put the logs in one file
+#[373][owen.Thesmo: Hierapolis]$ find ./podcast-logs/ -name "*" | wc -l
+# Will need to use find and xargs to avoid terminal count problems
+
+#cat $ROOT/sample/logs/* | $ROOT/scripts/cleanup.sh > $ROOT/sample/one-week-full-logs.txt
+# Finds all the items from 2012/2013... (to avoid finding non-log files)
+find . -name "*201*" | xargs cat | $ROOT/scripts/cleanup.sh > ./processed-logs.txt
+
+# Create a databse
+
+#Putting these in comments since I'm running by hand as a I don't yet know how to script sqlite3 actions
+#sqlite> create table s3stats0(requestid text,owner text,bucket text,time text,ip text,requesterid text,operation text,key text,uri text,httpstatus integer,error text,sentbytes integer,objectsize integer,totaltime integer, turnaroundtime integer, referrer text, useragent text, versionid text);
+#sqlite> .separator(' ')
+#Error: unknown command or invalid arguments:  "separator('". Enter ".help" for help
+#sqlite> .separator ' '
+#sqlite> .import ../
+#../.git       ../db         ../docs       ../README.md  ../sample     ../scripts  
+#sqlite> .import ../sample/one-week-full-logs.txt s3stats0
+#sqlite> select * from s3stats0 limit 5
+
+# Read the file into the database
+
+# Generate a website with stats and graphs from the db
